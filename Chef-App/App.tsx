a44877b6e8ef255d7c1fee2f2a13e5ca1995d15c
hp <ben.blessing2006@gmail.com>
2025-11-11 22:16:20 +0200

    Added icons to the tab navigation

--- Chef-App/App.tsx
+++ Chef-App/App.tsx
@@ -1,40 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Welcome from './Screens/WelcomeChef';
 import Menu from './Screens/SecondScreen';
-import { Icon } from 'react-native-screens';
 import List from './Screens/Thirdscreen';
 import Filter from './Screens/Fourthscreen';
+import { Ionicons } from '@expo/vector-icons';
 
-
-const stack = createNativeStackNavigator()
-const Tab = createBottomTabNavigator()
+const stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <Tab.Navigator
-      screenOptions={{
-        tabBarActiveTintColor: "blue",
-        tabBarInactiveTintColor: "#000000fe",
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: '#000000fe',
         tabBarStyle: {
-          justifyContent: "center",
-          alignItems: "center",
-          borderColor: "black",
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderColor: 'black',
           borderWidth: 1,
+          paddingRight: 70,
           borderRadius: 10,
-          backgroundColor: "#43df43fe"
-        }
-      }}
+          backgroundColor: '#43df43fe',
+
+          paddingBottom: 6,
+        },
+        tabBarItemStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        tabBarIcon: ({ color, size }) => {
+          let iconName = 'ellipse';
+
+          if (route.name === 'Welcome') {
+            iconName = 'home-outline';
+          } else if (route.name === 'Menu') {
+            iconName = 'restaurant-outline';
+          } else if (route.name === 'List') {
+            iconName = 'list-outline';
+          } else if (route.name === 'Filter') {
+            iconName = 'filter-outline';
+          }
+
+          return <Ionicons name={iconName as any} size={24} color={color} style={{ textAlign: 'center' }} />;
+        },
+        headerShown: false,
+        tabBarLabelStyle: { fontSize: 12, textAlign: 'center' },
+      })}
     >
-      <Tab.Screen name="Welcome" component={Stacknavigator} options={{ tabBarButton: () => null }} />
-      <Tab.Screen name="Menu" component={Menu} />
-      <Tab.Screen name="List" component={List} options={{ tabBarButton: () => null }} />
-      <Tab.Screen name="Filter" component={Filter} />
+      <Tab.Screen name="Welcome" component={Stacknavigator} options={{ tabBarButton: () => null, tabBarLabel: 'Welcome' }} />
+      <Tab.Screen name="Menu" component={Menu} options={{ tabBarLabel: 'Menu' }} />
+      <Tab.Screen name="List" component={List} options={{ tabBarButton: () => null, tabBarLabel: 'List' }} />
+      <Tab.Screen name="Filter" component={Filter} options={{ tabBarLabel: 'Filter' }} />
     </Tab.Navigator>
-  )
+  );
 }
 
 function Stacknavigator() {
@@ -42,16 +65,16 @@ function Stacknavigator() {
     <stack.Navigator>
       <stack.Screen name="Welcome1" component={Welcome} options={{ headerShown: false }} />
     </stack.Navigator>
-  )
+  );
 }
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <TabNavigator />
+      <StatusBar style="auto" />
     </NavigationContainer>
-  )
+  );
 }
 
 
